6c9a4a80676abeb2ee827d64de4b9374d8a501fe
Mahmoud Elkashef <mahmoud.gelkashef@hotmail.com>
2022-10-26 23:00:17 +0200

    Add docker-compose file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const PORT=4000;
 const app=express();
 
-app.get('/', (req,res)=>res.send(`<h1> Hello World! Hi Hi Hi Hi </h1>`))
+app.get('/', (req,res)=>res.send(`<h1> Hello World!</h1>`))
 app.listen(PORT,()=>console.log(`App is up and runing on port: ${PORT}`))
